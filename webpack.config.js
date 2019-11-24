c156d1e9bebe6ebb4e9f0fc55543995d7e03b28d
ddchef <o.o@xjie.me>
2019-11-24 22:33:04 +0800

    chore: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const Path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry:'./src/index.js',
+  entry:{
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output:{
-    filename:'bundle.js',
+    filename:'[name].bundle.js',
     path: Path.resolve(__dirname,'./dist')
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title:'vue-webpack'
+    })
+  ],
   module:{
     rules:[
       {
@@ -20,7 +29,13 @@ module.exports = {
           'file-loader'
         ]
       },
-      
+      {
+        test:/\.(woff|woff2|eot|ttf|otf)$/,
+        use:[
+          'file-loader'
+        ]
+      },
+
     ]
   }
 }
