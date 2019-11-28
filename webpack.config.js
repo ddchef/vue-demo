c7e2c6d97bf21cb91376dfe8b270459ef4db0bf6
ddchef <o.o@xjie.me>
2019-11-28 21:22:26 +0800

    chore: add vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,12 @@ module.exports = {
   module:{
     rules:[
       {
+        test:/\.vue$/,
+        use:[
+          'vue-loader'
+        ]
+      },
+      {
         test:/\.css$/,
         use:[
           'style-loader',
