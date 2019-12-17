5fe44c5fea33eb40f7f5d83207244db022c8e96f
ddchef <o.o@xjie.me>
2019-12-17 23:42:38 +0800

    refactor: 调整首页样式

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <h1>hello Vue</h1>
+  <div id="app">
+    <h1>Hello Vue</h1>
     <img :src="src">
   </div>
 </template>
 <script>
 import img from './asset/img/cola.png'
+import './asset/style/index.css'
 export default {
   name: 'App',
   data () {
